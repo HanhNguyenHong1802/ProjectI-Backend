6f10f8f8bbf51b32bca499e9c127970f89be5868
hanhnguyenhong18022000 <64351493+hanhnguyenhong18022000@users.noreply.github.com>
2020-12-15 23:02:37 +0700

    Update comments.js
    comment to explain

--- models/comments.js
+++ models/comments.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-	rating: {
-		type: Number,
-		min: 0,
+const commentSchema = new Schema({                  //Khi khởi tạo new Schemas là lập trình viên 
+	rating: {										//đang khởi tạo một khuôn mẫu của collection
+		type: Number,								//giống như trong các hệ quản trị cơ sở dữ liệu
+		min: 0,										//quan hệ (RDBMS)
 		max: 5,
-		required: true
-	},
-	content: {
+		required: true								
+	},												//Một Schema thể hiện các document trong collection 
+	content: {										//chỉ định sẽ có cấu trúc như thế nào
 		type: String,
 		default: ''
 	},
-	author:{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
-	},
-	drink:{
+	author:{										//Schema chịu trách nhiệm cho việc các collection của 
+		type: mongoose.Schema.Types.ObjectId,		//sản phẩm sẽ có hình dạng ra sao, tạo ra các khung  
+		ref: 'User'									//khuôn mẫu của dữ liệu, thêm vào đó nó còn cho phép 
+	},												//lập trình viên kiểm tra dữ liệu có đúng kiểu vốn có 
+	drink:{											//của chúng hay không
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'Drink'
 	}
@@ -24,6 +24,11 @@ const commentSchema = new Schema({
 	tiemstamps:{}
 });
 
-var Comments = mongoose.model('Comment', commentSchema);
-module.exports = Comments;
-
+var Comments = mongoose.model('Comment', commentSchema);		//Model là kết quả từ quá trình compile 
+module.exports = Comments;										//Schema đã được định nghĩa trước đó.	
+																//Một thể hiện của model được gọi là document.
+																//Model sẽ chịu trách nhiện chính trong 
+																//việc đọc và ghi 
+																//dữ liệu trong việc đọc và ghi dữ liệu từ 
+																// database MongoDB
+																
