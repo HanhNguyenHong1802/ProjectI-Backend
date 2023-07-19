fe113ed26a49c1333cfee40cc5945fe8c7412444
hanh2112 <hanhnguyee2000@gmail.com>
2023-07-20 00:50:04 +0700

    Add price

--- models/drinks.js
+++ models/drinks.js
@@ -2,31 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const drinkSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	type: {
-		type: String,
-		default: ''
-	},
-	description:{
-		type: String,
-		default: ''
-	},
-	image:{
-		type: String,
-		required: true
-	},
-	recommended:{
-		type: Boolean,
-		default:false
-	}
-},{
-	tiemstamps:{}
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  type: {
+    type: String,
+    default: ''
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  recommended: {
+    type: Boolean,
+    default: false
+  },
+  price: {
+    type: number,
+    min: 1000, 
+    default:0,
+  }
+}, {
+  tiemstamps: {}
 });
 
-var Drinks = mongoose.model('Drink',drinkSchema);
+var Drinks = mongoose.model('Drink', drinkSchema);
 module.exports = Drinks;
 
