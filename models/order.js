4e8c85c372c4b250717848913cef063a867dd8ed
Hanh Nguyen Hong <hanhnguyee2000@gmail.com>
2023-07-24 18:00:16 +0700

    Fix order

--- models/order.js
+++ models/order.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose')
-const Members = require('./members');
-const Drinks = require('./drinks');
+const mongoose = require("mongoose");
+const Drinks = require("./drinks");
+const User = require("./users");
+const Schema = mongoose.Schema;
 
-const Schema = mongoose.Schema
-
-const orderSchema = new Schema({
-  customerId: {
-    type: mongoose.Schema.Types.ObjectId,
-		ref: 'User'
+const orderSchema = new Schema(
+  {
+    author: {
+      type: String,
+      require: true,
+    },
+    table: {
+      type: Number,
+      require: true,
+    },
+    orders: [Drinks.schema],
+    totalAmount: {
+      type: Number,
+      require: true,
+      min: 0,
+    },
+    paid: {
+      type: Boolean,
+      default: false,
+    },
   },
-  table: {
-    type: Number,
-    require: true
-  },
-  orders: [Drinks.schema],
-  totalAmount: {
-    type: Number,
-    require: true,
-    min: 0
-  },
-  paid: {
-    type: Boolean,
-    default: false
-  }
-}, { timestamps: true }
-)
-var Orders = mongoose.model('Order', orderSchema);
+  { timestamps: true }
+);
+var Orders = mongoose.model("Order", orderSchema);
 module.exports = Orders;
